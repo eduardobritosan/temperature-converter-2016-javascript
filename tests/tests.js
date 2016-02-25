34ef8eda53eddc10a0b39260a078b609c652307d
Eldon <alu0100777758@edu.ull.es>
2016-02-25 20:36:53 +0000

    Added new tests

--- tests/tests.js
+++ tests/tests.js
@@ -16,5 +16,24 @@ suite('temperature', function() {
         calculate();
         assert.match(converted.innerHTML, /ERROR/);
     });
+    test('32c = 89.6 Farenheit', function() {
+        original.value = "32c";
+        calculate();
+        assert.match(converted.innerHTML,"89.6 Farenheit");
+    });
+    test('32ce = 89.6 Farenheit', function() {
+        original.value = "32ce";
+        calculate();
+        assert.match(converted.innerHTML,"89.6 Farenheit");
+    });
+    test('32cel = 89.6 Farenheit', function() {
+        original.value = "32cel";
+        calculate();
+        assert.match(converted.innerHTML,"89.6 Farenheit");
+    });
+    test('32celf = 89.6 Farenheit', function() {
+        original.value = "32celf";
+        calculate();
+        assert.match(converted.innerHTML,/ERROR/);
+    });
 });
-
